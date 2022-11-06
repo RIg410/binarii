5b6ae8d479df9969949eed7cbb78b1bec905fa5d
rig410 <rig410@yandex.ru>
2022-11-06 22:26:36 +0300

    byte flip flop

--- src/elements/complex.rs
+++ src/elements/complex.rs
@@ -79,7 +79,7 @@ impl Complex {
         }
     }
 
-    pub fn add_key(&mut self, key: Gate) -> usize {
+    pub fn add_gate(&mut self, key: Gate) -> usize {
         let id = self.gates.len();
         self.gates.push(Element::Gate(key));
         self.compile();
@@ -314,7 +314,7 @@ pub mod tests {
         complex.add_input(wire_1);
         complex.add_input(wire_2);
         complex.add_output(wire_out);
-        complex.add_key(key);
+        complex.add_gate(key);
         complex
     }
 
@@ -327,7 +327,7 @@ pub mod tests {
         complex.add_input(wire_1);
         complex.add_input(wire_2);
         complex.add_output(wire_out);
-        complex.add_key(key);
+        complex.add_gate(key);
         complex
     }
 
@@ -338,12 +338,12 @@ pub mod tests {
         let key = Gate::not(wire_1.clone(), wire_out.clone());
         complex.add_input(wire_1);
         complex.add_output(wire_out);
-        complex.add_key(key);
+        complex.add_gate(key);
         complex
     }
 
-    pub fn rs_trigger() -> Complex {
-        let mut complex = Complex::new("rs_trigger");
+    pub fn rs_flip_flop() -> Complex {
+        let mut complex = Complex::new("rs_flip_flop");
         let wire_r = Wire::new();
         let wire_s = Wire::new();
         let wire_q = Wire::new();
@@ -354,15 +354,15 @@ pub mod tests {
         complex.add_output(wire_q.clone());
         complex.add_output(wire_qn.clone());
 
-        complex.add_key(Gate::nor(wire_r.clone(), wire_qn.clone(), wire_q.clone()));
-        complex.add_key(Gate::nor(wire_s.clone(), wire_q.clone(), wire_qn.clone()));
+        complex.add_gate(Gate::nor(wire_r.clone(), wire_qn.clone(), wire_q.clone()));
+        complex.add_gate(Gate::nor(wire_s.clone(), wire_q.clone(), wire_qn.clone()));
 
         complex
     }
 
     #[test]
-    pub fn test_rs_trigger() {
-        let rs = rs_trigger();
+    pub fn test_rs_flip_flop() {
+        let rs = rs_flip_flop();
 
         let set = rs.get_in(0);
         let reset = rs.get_in(1);
@@ -457,8 +457,8 @@ pub mod tests {
         and_3.add_output(wire_out.clone());
 
         let inner = Wire::new();
-        and_3.add_key(Gate::and(and_3.get_in(0), and_3.get_in(1), inner.clone()));
-        and_3.add_key(Gate::and(inner, and_3.get_in(2), and_3.get_out(0)));
+        and_3.add_gate(Gate::and(and_3.get_in(0), and_3.get_in(1), inner.clone()));
+        and_3.add_gate(Gate::and(inner, and_3.get_in(2), and_3.get_out(0)));
         and_3.conduct();
         assert_eq!(and_3.get_out(0).get(), false);
 
@@ -488,8 +488,8 @@ pub mod tests {
         complex.add_output(wire_s.clone());
         complex.add_output(wire_c.clone());
 
-        complex.add_key(Gate::xor(wire_a.clone(), wire_b.clone(), wire_s.clone()));
-        complex.add_key(Gate::and(wire_a.clone(), wire_b.clone(), wire_c.clone()));
+        complex.add_gate(Gate::xor(wire_a.clone(), wire_b.clone(), wire_s.clone()));
+        complex.add_gate(Gate::and(wire_a.clone(), wire_b.clone(), wire_c.clone()));
 
         complex
     }
@@ -523,7 +523,7 @@ pub mod tests {
 
         let wire_c_2 = half_sum_2.get_out(1);
 
-        complex.add_key(Gate::or(wire_c_1, wire_c_2, wire_c.clone()));
+        complex.add_gate(Gate::or(wire_c_1, wire_c_2, wire_c.clone()));
         complex.add_complex(half_sum_1);
         complex.add_complex(half_sum_2);
         complex
@@ -632,5 +632,141 @@ pub mod tests {
                 assert_eq!(carry, expected_carry);
             }
         }
+    }
+
+    pub fn d_flip_flop() -> Complex {
+        let mut flip_flop = Complex::new("d_flip_flop");
+        let d = Wire::new();
+        let clk = Wire::new();
+
+        flip_flop.add_input(d.clone());
+        flip_flop.add_input(clk.clone());
+
+        let reset = Wire::new();
+        flip_flop.add_gate(Gate::and(clk.clone(), d.clone(), reset.clone()));
+
+        let not_data = Wire::new();
+        flip_flop.add_gate(Gate::not(d.clone(), not_data.clone()));
+
+        let set = Wire::new();
+        flip_flop.add_gate(Gate::and(clk.clone(), not_data.clone(), set.clone()));
+
+        let mut rs = rs_flip_flop();
+        rs.set_in(1, set.clone());
+        rs.set_in(0, reset.clone());
+
+        let q = rs.get_out(0);
+        let nq = rs.get_out(1);
+        flip_flop.add_output(q.clone());
+        flip_flop.add_output(nq.clone());
+        flip_flop.add_complex(rs);
+        flip_flop
+    }
+
+    #[test]
+    pub fn test_d_flip_flop() {
+        let flip_flop = d_flip_flop();
+        let d = flip_flop.get_in(0);
+        let clk = flip_flop.get_in(1);
+        let q = flip_flop.get_out(0);
+        let nq = flip_flop.get_out(1);
+        clk.set(true);
+        flip_flop.conduct();
+        clk.set(false);
+        flip_flop.conduct();
+        println!("{}", flip_flop);
+        assert_eq!(q.get(), false);
+        assert_eq!(nq.get(), true);
+
+        d.set(true);
+        flip_flop.conduct();
+        assert_eq!(q.get(), false);
+        assert_eq!(nq.get(), true);
+
+        d.set(false);
+        flip_flop.conduct();
+        assert_eq!(q.get(), false);
+        assert_eq!(nq.get(), true);
+        d.set(true);
+        clk.set(true);
+        flip_flop.conduct();
+        assert_eq!(q.get(), true);
+        assert_eq!(nq.get(), false);
+        clk.set(false);
+        d.set(false);
+        flip_flop.conduct();
+        assert_eq!(q.get(), true);
+        assert_eq!(nq.get(), false);
+        clk.set(true);
+        flip_flop.conduct();
+        assert_eq!(q.get(), false);
+        assert_eq!(nq.get(), true);
+    }
+
+    pub fn byte_flip_flop() -> Complex {
+        let mut flip_flop = Complex::new("byte_flip_flop");
+
+        let d_in = Bus::new(8);
+        let clk = Wire::new();
+
+        let mut q_out = Bus::new(8);
+        let clk_out = clk.clone();
+
+        for i in 0..8 {
+            let mut dff = d_flip_flop();
+            dff.set_in(0, d_in.get_wire(i));
+            dff.set_in(1, clk.clone());
+            q_out.set_wire(i, dff.get_out(0));
+            flip_flop.add_complex(dff);
+        }
+        flip_flop.add_input_bus(d_in);
+        flip_flop.add_input(clk);
+        flip_flop.add_output_bus(q_out);
+        flip_flop.add_output(clk_out);
+
+        flip_flop
+    }
+
+    #[test]
+    pub fn test_byte_flip_flop() {
+        let flip_flop = byte_flip_flop();
+        let d_in = flip_flop.get_in_bus(0, 8);
+        let clk = flip_flop.get_in(8);
+
+        let q_out = flip_flop.get_out_bus(0, 8);
+        let clk_out = flip_flop.get_out(8);
+        clk.set(true);
+        flip_flop.conduct();
+        clk.set(false);
+        flip_flop.conduct();
+
+        let data: u8 = q_out.get(0);
+        assert_eq!(data, 0);
+        d_in.set(0, 0b11111111);
+        flip_flop.conduct();
+        let data: u8 = q_out.get(0);
+        assert_eq!(data, 0);
+
+        clk.set(true);
+        flip_flop.conduct();
+        let data: u8 = q_out.get(0);
+        assert_eq!(data, 0b11111111);
+
+        clk.set(false);
+        flip_flop.conduct();
+        let data: u8 = q_out.get(0);
+        assert_eq!(data, 0b11111111);
+
+        d_in.set(0, 0b10101010);
+        flip_flop.conduct();
+        let data: u8 = q_out.get(0);
+        assert_eq!(data, 0b11111111);
+
+        clk.set(true);
+        flip_flop.conduct();
+        clk.set(false);
+        flip_flop.conduct();
+        let data: u8 = q_out.get(0);
+        assert_eq!(data, 0b10101010);
     }
 }
